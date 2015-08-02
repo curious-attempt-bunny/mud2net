57fffc934e845734f700df406a979e81863de272
Merlyn <curious.attempt.bunny@gmail.com>
2015-08-01 17:27:23 -0700

    Render the reset times.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject mud2net "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring "1.4.0"]]
+                 [ring "1.4.0"]
+                 [hiccup "1.0.5"]]
   :resource-paths ["lib/jta26.jar"]
   :main ^:skip-aot mud2net.core
   :target-path "target/%s"
--- src/mud2net/core.clj
+++ src/mud2net/core.clj
@@ -1,17 +1,87 @@
 (ns mud2net.core
-  (:use ring.adapter.jetty)
+  (:use ring.adapter.jetty hiccup.core mud2net.mudtime)
   (:gen-class))
 
-(defn handler [request]
-      {:status  200
-       :headers {"Content-Type" "text/html"}
-       :body    "Hallo leute!"})
-
-(defn times [request]
-  {:status  200
-   :headers {"Content-Type" "appl"}
-   :body    "Hallo leute!"})
-
-(defn -main
+(defn page
   []
-  (run-jetty handler {:port 3000}))
+  (let [data   (reset-data)
+        number (:number data)
+        time   (.toEpochSecond (:time data))]
+    [:html
+     [:head
+      [:script {:src "http://code.jquery.com/jquery-2.1.4.js"}]
+      [:script (str "start_time = " (* 1000 time) ";" "\n" "reset_number = " number ";")]
+      [:script
+       "$(document).ready(function() {
+        $('.time').each(function(k,tag) {
+          $(tag).append(new Date(start_time).toString());
+          start_time += 107 * 60 * 1000;
+        });
+        $('.number').each(function(k,tag) {
+          $(tag).append(reset_number);
+          reset_number += 1;
+        });
+       });"]]
+     [:body
+      [:table
+       [:thead
+        [:tr
+         [:th "Reset number"]
+         [:th "Starts at"]
+        ]
+       ]
+       [:tbody
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        [:tr
+         [:td {:class "number"}]
+         [:td {:class "time"}]]
+        ]
+       ]
+      ]
+     ]))
+
+(defn app [request]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (html (page))})
+
+(def server (run-jetty app {:port 3000}))
+
