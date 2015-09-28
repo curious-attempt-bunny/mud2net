5aa48dcf40cd2339b353569b213ac6df1c70dcfb
Merlyn <curious.attempt.bunny@gmail.com>
2015-09-27 20:41:10 -0700

    Add New Relic instrumentation.

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring "1.4.0"]
                  [hiccup "1.0.5"]]
-  :resource-paths ["lib/jta26.jar", "resources"]
+  :resource-paths ["lib/jta26.jar", "lib/newrelic.jar","resources"]
+  :jvm-opts ["-javaagent:lib/newrelic.jar"]
   :main ^:skip-aot mud2net.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
